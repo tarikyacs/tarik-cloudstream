f2697efc86dc0026a2c2d057736ff13a06d8bdad
tarikyacs <205378099+tarikyacs@users.noreply.github.com>
2026-09-19 20:41:01 +0000

    ♻️ Domain Değişikliği
    🔄 Otomatik domain güncellemeleri yapıldı.

--- SezonlukDizi/src/main/kotlin/com/keyiflerolsun/SezonlukDizi.kt
+++ SezonlukDizi/src/main/kotlin/com/keyiflerolsun/SezonlukDizi.kt
@@ -13,7 +13,7 @@ import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 
 class SezonlukDizi : MainAPI() {
-    override var mainUrl = "https://sezonlukdizi6.com"
+    override var mainUrl = "https://sezonlukdizi.cc"
     override var name = "SezonlukDizi"
     override val hasMainPage = true
     override var lang = "tr"
@@ -221,11 +221,11 @@ class SezonlukDizi : MainAPI() {
         val header = mapOf(
             "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0",
             "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Referer" to "https://sezonlukdizi6.com/"
+            "Referer" to "https://sezonlukdizi.cc/"
         )
         val son = app.get(
             iframe,
-            referer = "https://sezonlukdizi6.com",
+            referer = "https://sezonlukdizi.cc",
             headers = header
         ).document.select("script").find { it.data().contains("function(p,a,c,k,e") }?.data() ?: ""
         val unPacked = JsUnpacker(son).unpack()
@@ -239,7 +239,7 @@ class SezonlukDizi : MainAPI() {
         /*val generateM3u8 = M3u8Helper.generateM3u8(
             "StreamRuby",
             sonFile.file,
-            "https://sezonlukdizi6.com/"
+            "https://sezonlukdizi.cc/"
         )*/
         callback.invoke(
             ExtractorLink(
